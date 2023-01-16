0bb2ef9ce6c96f5c15da9e0d4b2f21b9ad5462ec
Dihee <hwangjihee94@gmail.com>
2023-01-16 15:55:08 +0900

    chore: Custom Color / Font 정의

---
+++ Weather/Weather/Presentation/Common/Font.swift
@@ -0,0 +1,22 @@
+//
+//  Font.swift
+//  Weather
+//
+//  Created by Jihee hwang on 2023/01/16.
+//
+
+import Foundation
+
+struct Font {
+    enum Roboto {
+        static let bold = "Roboto-Bold"
+        static let medium = "Roboto-Medium"
+        static let regular = "Roboto-Regular"
+    }
+    
+    enum NotoSans {
+        static let bold = "NotoSansKR-Bold"
+        static let medium = "NotoSansKR-Medium"
+        static let regular = "NotoSansKR-Regular"
+    }
+}
---
+++ Weather/Weather/Presentation/Extension/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extension.swift
+//  Weather
+//
+//  Created by Jihee hwang on 2023/01/16.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backGroundBlue = UIColor(named: "backGroundBlue")
+    static let backGroundGray = UIColor(named: "backGroundGray")
+    static let line = UIColor(named: "line")
+    static let gradientBlue = UIColor(named: "gradientBlue") ?? UIColor(red: 72, green: 132, blue: 204, alpha: 1)
+    static let gradientPurple = UIColor(named: "gradientPurple") ?? UIColor(red: 142, green: 72, blue: 204, alpha: 1)
+}
