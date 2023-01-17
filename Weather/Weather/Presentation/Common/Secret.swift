839cc15fe46b0f706e51000a9b2bec6e45e9d7d4
Dihee <hwangjihee94@gmail.com>
2023-01-17 22:19:31 +0900

    feat: Secret 추가

---
+++ Weather/Weather/Presentation/Common/Secret.swift
@@ -0,0 +1,40 @@
+//
+//  Secret.swift
+//  Weather
+//
+//  Created by Jihee hwang on 2023/01/17.
+//
+
+import Foundation
+import RxSwift
+
+struct Secret {
+    private let keyChainRepository: KeyChainRepository
+    
+    private let account = "APIKey"
+    private let apiKey = "4f728cb2e78aa315e2e2ede5053d3861"
+    
+    init(keyChainRepository: KeyChainRepository) {
+        self.keyChainRepository = keyChainRepository
+    }
+    
+    func saveAPIKey() {
+        keyChainRepository.createKey(account: account, key: apiKey)
+    }
+    
+    func readAPIKey() -> String {
+        var key = String()
+        
+        keyChainRepository
+            .readKey(account: account)
+            .subscribe { result in
+                switch result {
+                case .success(let value):
+                    key = value
+                case .failure(_):
+                    fatalError("Fail to read API Key")
+                }
+            }
+        return key
+    }
+}
